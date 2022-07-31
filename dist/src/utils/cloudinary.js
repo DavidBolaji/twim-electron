e7a12a73d5a50bee324c0d4dfb7c5765969b35e0
DavidBolaji <odavidbolaji14@gmail.com>
2022-07-31 19:22:34 +0100

    cloudinary config added

--- dist/src/utils/cloudinary.js
+++ dist/src/utils/cloudinary.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const config_1 = __importDefault(require("config"));
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
-    cloud_name: config_1.default.get('cloudinaryCloudName') || process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: config_1.default.get('cloudinaryApiKey') || process.env.CLOUDINARY_API_KEY,
-    api_secret: config_1.default.get('cloudinaryApiSecret') || process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || config_1.default.get('cloudinaryCloudName'),
+    api_key: process.env.CLOUDINARY_API_KEY || config_1.default.get('cloudinaryApiKey'),
+    api_secret: process.env.CLOUDINARY_API_SECRET || config_1.default.get('cloudinaryApiSecret'),
 });
 module.exports = { cloudinary };
--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -2,9 +2,9 @@ import config from "config";
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: config.get('cloudinaryCloudName') || process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: config.get('cloudinaryApiKey') || process.env.CLOUDINARY_API_KEY,
-    api_secret: config.get('cloudinaryApiSecret') || process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || config.get('cloudinaryCloudName'),
+    api_key: process.env.CLOUDINARY_API_KEY || config.get('cloudinaryApiKey'),
+    api_secret: process.env.CLOUDINARY_API_SECRET || config.get('cloudinaryApiSecret'),
 });
 
 module.exports = { cloudinary }
